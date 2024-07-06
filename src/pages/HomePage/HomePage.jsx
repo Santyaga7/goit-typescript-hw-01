64c7a4d66f2c2b91792851a6d961005c6081bbf3
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-07-06 12:34:48 +0300

    Initial commit

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,4 @@
+import style from "./HomePage.module.css"
+export default function HomePage() {
+  return <h2 className={style.titlePage}>👉 Welcome to the contact book page. Now your contacts are always at hand!👈</h2>;
+}
